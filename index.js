b07db0fc91f1cdb3f4180408fc79d93744d13764
Lskeey <muhammd.alviansyah@gmail.com>
2024-10-16 12:16:35 +0700

    Feat: add url validation for user input

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const dns = require("dns");
 const app = express();
+
+function isValidHttpUrl(str) {
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$", // fragment locator
+    "i"
+  );
+  return pattern.test(str);
+}
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
@@ -14,9 +31,27 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+app.post("/api/shorturl", (req, res) => {
+  const input = req.body.url;
+  if (!input || input.trim() === "") {
+    return res.json({ error: "invalid url" });
+  }
+
+  if (!isValidHttpUrl(input)) {
+    return res.json({ error: "invalid url" });
+  }
+
+  try {
+    const url = new URL(req.body.url);
+    dns.lookup(url.hostname, (error) => {
+      if (error) {
+        return res.json({ error: "invalid url" });
+      }
+      res.json({ original_url: url });
+    });
+  } catch (error) {
+    res.json({ error: "invalid url" });
+  }
 });
 
 app.listen(port, function () {
